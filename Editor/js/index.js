acb2f3c7195a390c72f352371390b845662e1e6f
Alen Kaucic <alen.kaucic@student.um.si>
2022-05-17 16:51:49 +0200

    input polja

--- Editor/js/index.js
+++ Editor/js/index.js
@@ -1,31 +1,44 @@
-<<<<<<< HEAD:index.js
-
-   var input = document.createElement("input");
-   var text_editor = document.getElementById('text_editor');
-   document.addEventListener('dragstart', function (event) {
-   event.dataTransfer.setData("text/html", input.outerHTML);
-   
-=======
+/*
 document.addEventListener('dragstart', function (event) {
-    event.dataTransfer.setData('Text', event.target.innerHTML);
-    //event.dataTransfer.setData('Text', prozi());
-    //prozi();
-    
->>>>>>> 67b3baee9e54e3da79dd4d66c99f15b5f05e53f3:Engine/js/index.js
-  });
+    //event.dataTransfer.setData('Text', event.target.innerHTML);
+    event.dataTransfer.setData('Text', "")
+    //event.dataTransfer.setData('Text/plain', event.target.innerHTML);
 
-  document.addEventListener('drop', function (event) {
-   prompt("!23");
-   
-  });
-  function myFunction(){
-   setEndOfContenteditable(text_editor);
-  }  
+});
 
-  function setEndOfContenteditable(contentEditableElement)
-{
-    var range,selection;
-    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+document.addEventListener('drop', function (event) {
+    //prompt("!23");
+    prozi();
+
+});
+*/
+
+dragStart = (event) => {
+    console.log("Started dragging");
+    event.dataTransfer.setData("text", event.target.className);
+    console.log("Started dragging the element with id: " + event.dataTransfer.getData("Text"));
+    dropEvent();
+}
+
+dropEvent = (event) => {
+    let id = event.dataTransfer.getData("Text");
+    console.log("id elementa: " + id)
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  
+
+
+function myFunction() {
+    setEndOfContenteditable(text_editor);
+}
+
+function setEndOfContenteditable(contentEditableElement) {
+    var range, selection;
+    if (document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
     {
         range = document.createRange();//Create a range (a range is a like the selection but invisible)
         range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
@@ -34,8 +47,8 @@ document.addEventListener('dragstart', function (event) {
         selection.removeAllRanges();//remove any selections already made
         selection.addRange(range);//make the range you have just created the visible selection
     }
-    else if(document.selection)//IE 8 and lower
-    { 
+    else if (document.selection)//IE 8 and lower
+    {
         range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
         range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
         range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
@@ -44,14 +57,17 @@ document.addEventListener('dragstart', function (event) {
 }
 
 
-<<<<<<< HEAD:index.js
-=======
- function prozi(){
-    let nekaj = document.getElementById("text");
+function prozi() {
+    let nekaj = document.getElementById("text_editor");
     var input = document.createElement("input");
     input.type = "text";
     input.className = "css-class-name"; // set the CSS class
-    input.setAttribute("placeholder", "Vnosno polje")
+    input.setAttribute("placeholder", placeholder()); //set placeholder text 
     nekaj.appendChild(input); // put it into the DOM
- }
->>>>>>> 67b3baee9e54e3da79dd4d66c99f15b5f05e53f3:Engine/js/index.js
+}
+
+
+placeholder = () => {
+    let customText = prompt("Vnesi poljubno vprašanje");
+    return customText;
+}
