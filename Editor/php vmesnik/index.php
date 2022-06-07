d0b2d3b79233d8b58477949bab62f3f1d1e57ad3
LiamG <liamgalof@gmail.com>
2022-06-07 22:07:13 +0200

    www

--- Editor/php vmesnik/index.php
+++ Editor/php vmesnik/index.php
@@ -101,7 +101,7 @@ session_start();
                  
                 </ul>
                 <button type="button" onclick="get_editor_content()">Shrani tekst</button>
-                <br
+                <br>
                 <button type="button" onclick="set_editor_content()">Naloži tekst</button>
              </div>
         </aside>
@@ -116,9 +116,9 @@ session_start();
           <button type="button" @click="showModal = !showModal" class="invisible" id="gumb123">Open Modal</button>
   
           
-          <div x-show="showModal" class="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-black bg-opacity-40 left-0 right-0 top-0 bottom-0" x-transition:enter="transition ease duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+          <div x-show="showModal" class="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-indigo-600 bg-opacity-5 left-0 right-0 top-0 bottom-0" x-transition:enter="transition ease duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
               
-              <div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 sm:w-10/12 mx-10" @click.away="showModal = false;delete_input()" x-transition:enter="transition ease duration-100 transform" x-transition:enter-start="opacity-0 scale-90 translate-y-1" x-transition:enter-end="opacity-100 scale-100 translate-y-0" x-transition:leave="transition ease duration-100 transform" x-transition:leave-start="opacity-100 scale-100 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-y-1">
+              <div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 sm:w-3/12 mx-10" @click.away="showModal = false;delete_input()" x-transition:enter="transition ease duration-100 transform" x-transition:enter-start="opacity-0 scale-70 translate-y-1" x-transition:enter-end="opacity-80 scale-100 translate-y-0" x-transition:leave="transition ease duration-100 transform" x-transition:leave-start="opacity-70 scale-100 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-y-1">
                  
                   <span class="font-bold block text-2xl mb-3">Vpišite vprašanje</span>
                   
--- Engine/php/config.php
+++ Engine/php/config.php
@@ -2,7 +2,7 @@
 
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpass = "root";
+$dbpass = "";
 $dbname = "pametni_obrazci";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
