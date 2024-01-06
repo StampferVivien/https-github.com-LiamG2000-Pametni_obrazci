0fc5a17ca9cb21b8c0a08b8ec3e8709c0f61b82e
Vivien <vivien.stampfer@gmail.com>
2024-01-06 13:04:28 +0100

    popravek

--- Editor/php_vmesnik/datoteke.php
+++ Editor/php_vmesnik/datoteke.php
@@ -21,12 +21,12 @@ $user_data = check_login($con);
     <body>
     
         <?php
-        include ("header.php");
-        include ("navBar.php");
+        include "header.php";
+        include "navBar.php";
         getUserDoc($con, $user_data["id"]);
         ?> 
          
-        <?php include ("footer.php"); ?>
+        <?php include "footer.php"; ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js " integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0 " crossorigin="anonymous "></script>
     </body>
 </html>
